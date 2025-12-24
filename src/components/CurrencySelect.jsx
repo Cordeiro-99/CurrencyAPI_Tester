d82fe7fa4e9ffa654259dbffeea06b15cbc060be
Cordeiro-99 <brunocor1999@gmail.com>
2025-12-24 17:12:35 +0000

    .

---
+++ src/components/CurrencySelect.jsx
@@ -0,0 +1,10 @@
+export default function CurrencySelect() {
+  return (
+    <select className="w-full border rounded-lg p-3 focus:ring-2 focus:ring-indigo-500 outline-none">
+      <option>EUR</option>
+      <option>USD</option>
+      <option>GBP</option>
+      <option>JPY</option>
+    </select>
+  );
+}
